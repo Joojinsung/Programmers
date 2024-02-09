2b6f7ff8ab3d2b274d2d69f29010bb0b833fc183
jinsung1017 <jinsung101707@gmail.com>
2024-02-09 16:23:47 +0900

    exhustive

---
+++ src/Exhaustive_Search/solution_1.java
@@ -0,0 +1,27 @@
+package Exhaustive_Search;
+
+public class solution_1 {
+    public static void main(String[] args) {
+        int[][] cards = {
+                {60, 50},
+                {30, 70},
+                {60, 30},
+                {80, 40}
+        };
+        int widthMax = 0;
+        int hightMax = 0;
+
+        for (int[] card : cards) {
+            int max = Math.max(card[0], card[1]);
+            int min = Math.min(card[0], card[1]);
+
+
+            widthMax = Math.max(widthMax, max);
+            hightMax = Math.max(hightMax, min);
+
+        }
+        System.out.println(widthMax * hightMax);
+
+    }
+
+}
