419eca6df4ca8c70770285bc3718ad865832cf1b
Joo-jinsung <dev.jinsung1017@gmail.com>
2024-02-15 22:35:14 +0900

    moo~

---
+++ src/baekjoon/Bronze/Moo.java
@@ -0,0 +1,10 @@
+//package baekjoon.Bronze;
+//
+//public class Moo {
+//    "(___)
+//            (o o)____/
+//    @@      \\
+//            \\ ____,/
+//            //   //
+//            ^^   ^^"
+//}
